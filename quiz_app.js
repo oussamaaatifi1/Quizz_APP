b44c5516cf9571938efc63ed7a2ddf539ce7b546
oussamaaatifi1 <nisowpro@gmail.com>
2024-03-22 11:06:23 +0000

    modifier navbar centraliser logo et ajoute icon play home page

--- quiz_app.js
+++
@@ -1,9 +0,0 @@
-function startQuiz() {
-    // Hide the initial content
-    document.querySelector(".back1").style.display = "none";
-    document.querySelector("quiz").style.display = "block";
-}
-
-
-
-
